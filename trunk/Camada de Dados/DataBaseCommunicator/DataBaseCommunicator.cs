50dc623dc8a649497093338631561e9166d1081c
jbteixeir@gmail.com <jbteixeir@gmail.com@b9c6b160-377e-3425-3200-e48b6f13f55a>
2011-05-23 01:48:49 +0000

    DataBaseCommunicator operacional ;)

--- trunk/Camada de Dados/DataBaseCommunicator/DataBaseCommunicator.cs
+++ trunk/Camada de Dados/DataBaseCommunicator/DataBaseCommunicator.cs
@@ -9,20 +9,68 @@ namespace ETdA.Camada_de_Dados.DataBaseCommunicator
     class DataBaseCommunicator
     {
         //Variaveis de Instancia
-        private String serverName;
-        SqlConnection connection;
+        private String server;
+        private SqlConnection connection;
+        private String database;
 
         //Construtores
-        public DataBaseCommunicator(String server, SqlConnection con)
+        public DataBaseCommunicator(String server, SqlConnection connection)
         {
-            serverName = server;
-            connection = con;
+            this.server = server;
+            this.connection = connection;
         }
 
         //Métodos
 
-        void connect(String serverName, String userName, String password);
-        void disconnect();
+        /*
+         * Abre uma ligação com a base de dados
+         */
+        void connect(String server, String username, String password, String database)
+        {
+            connection = new SqlConnection( "user id=" + username + ";" +
+                                            "password=" + password + ";" +
+                                            "server=" + server + ";" +
+                                            "Trusted_Connection=yes;" +
+                                            "database=" + database + ";" +
+                                            "connection timeout=30");
+        }
+
+        /*
+         * Fecha uma ligação com a base de dados
+         */
+        void disconnect()
+        {
+            try
+            {
+                connection.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        /*
+         * Executa query's sem ser de leitura à base de dados
+         */
+        void query(string query)
+        {
+            SqlCommand command = new SqlCommand(query, connection);
+            command.ExecuteNonQuery();
+        }
+
+        /*
+         * Executa query's de leitura à base de dados
+         */
+        SqlDataReader readData(string query)
+        {
+            SqlDataReader reader = null;
+            SqlCommand command = new SqlCommand("select * from table",
+                                                     connection);
+            reader = command.ExecuteReader();
+            return reader;
+        }
+
         void insert(List<String> values, List<int> tableNumber);
 
     }
