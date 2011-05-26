2235389dce7f98f52a654f3fe6b6e5895b2cc0fe
rafaelcaldeirasilva <rafaelcaldeirasilva@b9c6b160-377e-3425-3200-e48b6f13f55a>
2011-05-26 20:42:11 +0000

    improved

--- Camada de Negócio/GestaodeAnalistas.cs
+++ Camada de Negócio/GestaodeAnalistas.cs
@@ -3,15 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+using ETdA.Camada_de_Dados.DataBaseCommunicator;
+
 namespace ETdA.Camada_de_Negócio
 {
-    class GestaodeAnalistas
-    {
-        //Métodos
-        void registaAnalista(String nome, String username, String password);
-        void login(String username, String password);
-        void removeAnalisa(String codAnalista);
-        void logout();
+	class GestaodeAnalistas
+	{
+		//Métodos
+		//void registaAnalista(String nome, String username, String password);
 
-    }
+		/* 
+		 * Faz login na base de dados
+		 * @return se nao conseguir fazer login retorna false
+		 */
+		public bool login(String username, String password)
+		{
+			return DataBaseCommunicator.connect("rocket-pc", username, password, "ETdA_" + username);
+
+
+		}
+		//void removeAnalisa(String codAnalista);
+		//void logout();
+
+	}
 }
