e821ae21d0bfe8d300ee7205e0cf176a20589776
rafaelcaldeirasilva <rafaelcaldeirasilva@b9c6b160-377e-3425-3200-e48b6f13f55a>
2011-05-26 22:43:44 +0000

    asd

--- trunk/Camada de Interface/InterfaceLogin.cs
+++ trunk/Camada de Interface/InterfaceLogin.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 
+using ETdA.Camada_de_Negócio;
+
 namespace ETdA.Camada_de_Interface
 {
     public partial class InterfaceLogin : Form
@@ -14,6 +16,35 @@ namespace ETdA.Camada_de_Interface
         public InterfaceLogin()
         {
             InitializeComponent();
+			label1.Enabled = false;
+			label2.Enabled = false;
+			label3.Enabled = false;
         }
+
+		private void Login_Click(object sender, EventArgs e)
+		{
+			string user = textBox1.Text;
+			string pass = maskedTextBox1.Text;
+
+			if (GestaodeAnalistas.login(user, pass))
+			{	//  Se consegue ligar a base de dados
+				this.Close();
+				umaClassePorreira.Main();
+			}
+			else
+			{	//  Se nao tem connectividade a internet, ou o username ou a pass
+				// estao mal
+				label1.Enabled = true;
+				label2.Enabled = true;
+				label3.Enabled = true;
+			}
+
+		}
+
+
+		//private void InterfaceLogin_Load(object sender, EventArgs e)
+		//{
+
+		//}
     }
 }
