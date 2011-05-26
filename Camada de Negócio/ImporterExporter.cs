c4af95d8d41872df16f92d51c894c3075d0f6d57
rafaelcaldeirasilva <rafaelcaldeirasilva@b9c6b160-377e-3425-3200-e48b6f13f55a>
2011-05-26 18:24:47 +0000

    funcao de importar pronta

--- Camada de Negócio/ImporterExporter.cs
+++ Camada de Negócio/ImporterExporter.cs
@@ -1,13 +1,84 @@
-﻿using System;
+﻿
+using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace ETdA.Camada_de_Negócio
+//The csv reader
+using LumenWorks.Framework.IO.Csv;
+
+
+namespace CamadaNegocio
 {
     class ImporterExporter
     {
-        void importaFicheiro(String datapathFile);
-        void exportaParaFicheiro(String datapathFile, List<Formulario> formulario);
+		public static LinkedList<string[]> importaFicheiro(String datapathFile, bool temCabecalho, ref string[] cabecalhos)
+        {
+            #region normalAproach
+			
+			LinkedList<string[]> dados = new LinkedList<string[]>();
+			string linha;
+			StreamReader tr = new StreamReader(datapathFile);
+
+			char[] separator = { ';' };
+			if (temCabecalho)
+			{
+				// le a primeira linha
+				linha = tr.ReadLine();
+				cabecalhos = linha.Split(separator);
+			}
+
+			// le a primeira linha de dados
+			while((linha = tr.ReadLine()) != null)
+			{
+				string[] terms = linha.Split(separator);
+				string[] aux = new string[terms.Length];
+				for (int i = 0; i < terms.Length; i++)
+					aux[i] = terms[i];
+
+				dados.AddLast(aux);
+			}
+
+			// close the stream
+			tr.Close();
+
+            #endregion
+
+			#region //CSVReader
+			//// criar um CSV Reader
+			//CsvReader csv = new CsvReader(
+			//    new StreamReader(datapathFile), true, ';');
+
+			//int fieldCount = csv.FieldCount;
+			//string[] headers = csv.GetFieldHeaders();
+			//string[][] dados = new string[fieldCount][];
+			//int j = 0;
+
+			////Console.Beep(600, 5000);
+			//Console.WriteLine("-------------------------------------------------------------------------------------------------------------------");
+			//Console.WriteLine(fieldCount);
+			//Console.WriteLine(headers);
+			//Console.WriteLine("-------------------------------------------------------------------------------------------------------------------");
+
+			//while (csv.ReadNextRecord())
+			//{
+			//    for (int i = 0; i < fieldCount; i++)
+			//    {
+			//        dados[j][i] = csv[i];
+			//        Console.Write(string.Format("{0} = {1};", headers[i], csv[i]));
+			//        Console.WriteLine();
+			//    }
+			//    j++;
+			//}
+			#endregion
+
+			return dados;
+        }
+
+		/*public static void exportaParaFicheiro(String datapathFile, List<Formulario> formulario)
+		{
+
+		}*/
     }
 }
