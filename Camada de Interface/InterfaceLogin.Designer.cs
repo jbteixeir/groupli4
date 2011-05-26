a1d1e137bfa1212a228df84af1bdf787da7acd6b
rafaelcaldeirasilva <rafaelcaldeirasilva@b9c6b160-377e-3425-3200-e48b6f13f55a>
2011-05-26 22:43:44 +0000

    asd

--- Camada de Interface/InterfaceLogin.Designer.cs
+++ Camada de Interface/InterfaceLogin.Designer.cs
@@ -28,79 +28,118 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.Username = new System.Windows.Forms.Label();
-            this.Sair = new System.Windows.Forms.Button();
-            this.Login = new System.Windows.Forms.Button();
-            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
-            this.Password = new System.Windows.Forms.Label();
-            this.SuspendLayout();
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(73, 40);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(195, 20);
-            this.textBox1.TabIndex = 0;
-            // 
-            // Username
-            // 
-            this.Username.AutoSize = true;
-            this.Username.Location = new System.Drawing.Point(12, 40);
-            this.Username.Name = "Username";
-            this.Username.Size = new System.Drawing.Size(55, 13);
-            this.Username.TabIndex = 1;
-            this.Username.Text = "Username";
-            // 
-            // Sair
-            // 
-            this.Sair.Location = new System.Drawing.Point(192, 119);
-            this.Sair.Name = "Sair";
-            this.Sair.Size = new System.Drawing.Size(75, 23);
-            this.Sair.TabIndex = 2;
-            this.Sair.Text = "Sair";
-            this.Sair.UseVisualStyleBackColor = true;
-            // 
-            // Login
-            // 
-            this.Login.Location = new System.Drawing.Point(111, 118);
-            this.Login.Name = "Login";
-            this.Login.Size = new System.Drawing.Size(75, 23);
-            this.Login.TabIndex = 3;
-            this.Login.Text = "Login";
-            this.Login.UseVisualStyleBackColor = true;
-            // 
-            // maskedTextBox1
-            // 
-            this.maskedTextBox1.Location = new System.Drawing.Point(73, 67);
-            this.maskedTextBox1.Name = "maskedTextBox1";
-            this.maskedTextBox1.Size = new System.Drawing.Size(194, 20);
-            this.maskedTextBox1.TabIndex = 4;
-            // 
-            // Password
-            // 
-            this.Password.AutoSize = true;
-            this.Password.Location = new System.Drawing.Point(13, 67);
-            this.Password.Name = "Password";
-            this.Password.Size = new System.Drawing.Size(53, 13);
-            this.Password.TabIndex = 5;
-            this.Password.Text = "Password";
-            // 
-            // InterfaceLogin
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(289, 161);
-            this.Controls.Add(this.Password);
-            this.Controls.Add(this.maskedTextBox1);
-            this.Controls.Add(this.Login);
-            this.Controls.Add(this.Sair);
-            this.Controls.Add(this.Username);
-            this.Controls.Add(this.textBox1);
-            this.Name = "InterfaceLogin";
-            this.Text = "Login";
-            this.ResumeLayout(false);
-            this.PerformLayout();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.Username = new System.Windows.Forms.Label();
+			this.Sair = new System.Windows.Forms.Button();
+			this.Login = new System.Windows.Forms.Button();
+			this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+			this.Password = new System.Windows.Forms.Label();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(73, 158);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(195, 20);
+			this.textBox1.TabIndex = 0;
+			this.textBox1.Text = "test_user";
+			this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+			// 
+			// Username
+			// 
+			this.Username.AutoSize = true;
+			this.Username.Location = new System.Drawing.Point(12, 158);
+			this.Username.Name = "Username";
+			this.Username.Size = new System.Drawing.Size(55, 13);
+			this.Username.TabIndex = 1;
+			this.Username.Text = "Username";
+			// 
+			// Sair
+			// 
+			this.Sair.Location = new System.Drawing.Point(193, 250);
+			this.Sair.Name = "Sair";
+			this.Sair.Size = new System.Drawing.Size(75, 23);
+			this.Sair.TabIndex = 2;
+			this.Sair.Text = "Sair";
+			this.Sair.UseVisualStyleBackColor = true;
+			// 
+			// Login
+			// 
+			this.Login.Location = new System.Drawing.Point(112, 250);
+			this.Login.Name = "Login";
+			this.Login.Size = new System.Drawing.Size(75, 23);
+			this.Login.TabIndex = 3;
+			this.Login.Text = "Login";
+			this.Login.UseVisualStyleBackColor = true;
+			this.Login.Click += new System.EventHandler(this.Login_Click);
+			// 
+			// maskedTextBox1
+			// 
+			this.maskedTextBox1.Location = new System.Drawing.Point(73, 185);
+			this.maskedTextBox1.Name = "maskedTextBox1";
+			this.maskedTextBox1.PasswordChar = '*';
+			this.maskedTextBox1.Size = new System.Drawing.Size(194, 20);
+			this.maskedTextBox1.TabIndex = 4;
+			this.maskedTextBox1.Text = "password";
+			// 
+			// Password
+			// 
+			this.Password.AutoSize = true;
+			this.Password.Location = new System.Drawing.Point(13, 185);
+			this.Password.Name = "Password";
+			this.Password.Size = new System.Drawing.Size(53, 13);
+			this.Password.TabIndex = 5;
+			this.Password.Text = "Password";
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(70, 208);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(141, 13);
+			this.label1.TabIndex = 6;
+			this.label1.Text = "Não foi possível fazer login. ";
+			this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(70, 221);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(176, 13);
+			this.label2.TabIndex = 7;
+			this.label2.Text = "Verifique a sua ligação à internet, o ";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(83, 234);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(128, 13);
+			this.label3.TabIndex = 8;
+			this.label3.Text = "username ou a password.";
+			// 
+			// InterfaceLogin
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(279, 285);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.Password);
+			this.Controls.Add(this.maskedTextBox1);
+			this.Controls.Add(this.Login);
+			this.Controls.Add(this.Sair);
+			this.Controls.Add(this.Username);
+			this.Controls.Add(this.textBox1);
+			this.Name = "InterfaceLogin";
+			this.Text = "Login";
+			this.ResumeLayout(false);
+			this.PerformLayout();
 
         }
 
@@ -111,6 +150,9 @@
         private System.Windows.Forms.Button Sair;
         private System.Windows.Forms.Button Login;
         private System.Windows.Forms.MaskedTextBox maskedTextBox1;
-        private System.Windows.Forms.Label Password;
+		private System.Windows.Forms.Label Password;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
     }
 }
