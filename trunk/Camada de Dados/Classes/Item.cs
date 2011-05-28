645c9e89b68052d38be1348629cbce59cc9de7b6
jbteixeir@gmail.com <jbteixeir@gmail.com@b9c6b160-377e-3425-3200-e48b6f13f55a>
2011-05-28 22:32:41 +0000

    item:
    improve da classe item para passar a suportar as ponderaçoes e os limites entre as cores

--- trunk/Camada de Dados/Classes/Item.cs
+++ trunk/Camada de Dados/Classes/Item.cs
@@ -11,14 +11,30 @@ namespace ETdA.Camada_de_Dados.Classes
         private String codParametro;
         private String nomeParametro;
         private int defaultItem;
+        private float ponderacao_analista;
+        private float ponderacao_profissional;
+        private float ponderacao_cliente;
+        private float inter_vemelho;
+        private float inter_laranja;
+        private float inter_amarelo;
+        private float inter_verdelima;
+        private float inter_verde;
 
         //Constructores
 
-        public Item(String cod, String nome, int def)
+        public Item(String cod, String nome, int def, float ponderacao_analista, float ponderacao_profissional, float ponderacao_cliente, float inter_vemelho, float inter_laranja, float inter_amarelo, float inter_verdelima, float inter_verde)
         {
             codParametro = cod;
             nomeParametro = nome;
             defaultItem = def;
+            this.ponderacao_analista = ponderacao_analista;
+            this.ponderacao_profissional=ponderacao_profissional;
+            this.ponderacao_cliente = ponderacao_cliente;
+            this.inter_vemelho = inter_vemelho;
+            this.inter_laranja = inter_laranja;
+            this.inter_amarelo = inter_amarelo;
+            this.inter_verdelima = inter_verdelima;
+            this.inter_verde = inter_verde;
         }
 
         public Item()
@@ -26,13 +42,14 @@ namespace ETdA.Camada_de_Dados.Classes
             codParametro = "";
             nomeParametro = "";
             defaultItem = 0;
-        }
-
-        public Item(Item i)
-        {
-            codParametro = i.Codigo;
-            nomeParametro = i.Nome;
-            defaultItem = i.Default;
+            ponderacao_analista = 0.33f;
+            ponderacao_profissional = 0.33f;
+            ponderacao_cliente = 0.33f;
+            inter_vemelho = 1;
+            inter_laranja = 2;
+            inter_amarelo = 3;
+            inter_verdelima = 4;
+            inter_verde = 5;
         }
 
         //Métodos
@@ -51,5 +68,22 @@ namespace ETdA.Camada_de_Dados.Classes
             get { return defaultItem; }
             set { defaultItem = value; }
         }
+        
+        public float PonderacaoAnalista
+        {
+            get { return ponderacao_analista; }
+            set { ponderacao_analista = value; }
+        }
+        public float PonderacaoProfissional
+        {
+            get { return ponderacao_profissional; }
+            set { ponderacao_profissional = value; }
+        }
+        public float PonderacaoCliente
+        {
+            get { return ponderacao_cliente; }
+            set { ponderacao_cliente = value; }
+        }
+
     }
 }
