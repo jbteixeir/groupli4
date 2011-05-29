f732b3accee647d9eca26067e4c5649c10523be2
rafaelcaldeirasilva <rafaelcaldeirasilva@b9c6b160-377e-3425-3200-e48b6f13f55a>
2011-05-29 14:52:40 +0000

    criaAnalise

--- Camada de Negócio/GestaodeAnalises.cs
+++ Camada de Negócio/GestaodeAnalises.cs
@@ -2,17 +2,41 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+
 using ETdA.Camada_de_Dados.Classes;
+using ETdA.Camada_de_Dados.DataBaseCommunicator;
 
 namespace ETdA.Camada_de_Negócio
 {
     class GestaodeAnalises
     {
-//        //Métodos
+		enum TipoAnalise { AreaComum, Actividade, Zona };
+
+		//Métodos
+		private string getDescricao(TipoAnalise t)	{
+			switch (t)
+			{
+				case TipoAnalise.AreaComum:
+					return "'Area Comum'";
+				case TipoAnalise.Actividade:
+					return "'Actividade'";
+				case TipoAnalise.Zona:
+					return "'Zona'";
+				default:
+					return "";
+			}
+		}
+
         public static List<string> getNomeAnalises(){
             return Camada_de_Dados.ETdA.ETdA.Projecto.Analises;
         }
-//        void criaAnalise(String nomeAnalise);
+		public void criaAnalise(long codProjecto, String nomeAnalise, TipoAnalise tipo)
+		{
+			DateTime now = DateTime.Now;
+			DataBaseCommunicator.query("INSERT INTO analise VALUES ("+codProjecto+","
+				+ "CAST('"+now.Year+now.Month+now.Day+" 00:00:00' AS datetime), '"+
+				nomeAnalise+"', "+getDescricao(tipo)+",0,0,0);");
+		}
 //        void editaAnalise(String codAnalise, string nomeAnalise);
 //        void removeAnalise(String codAnalise);
 //        void adicionaItemAnalise(String codParametro);
